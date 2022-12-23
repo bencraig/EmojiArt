b1c09a5973d0aa26ba4f34323b14686659e699f4
Ben Craig <benjaminrichardcraig@gmail.com>
2022-12-23 08:38:14 -0600

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Benjamin Craig on 12/23/22.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
